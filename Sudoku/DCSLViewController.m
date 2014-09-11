82e9670895fb566d601a115b759b1f97c6dd6143
Daniel <Daniel@wl-214-91.wpa.claremont.edu>
2014-09-11 15:57:43 -0700

    Button 1

--- Sudoku/DCSLViewController.m
+++ Sudoku/DCSLViewController.m
@@ -8,7 +8,10 @@
 
 #import "DCSLViewController.h"
 
-@interface DCSLViewController ()
+@interface DCSLViewController (){
+    UIButton* _button1;
+
+}
 
 @end
 
@@ -18,7 +21,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGRect buttonFrame = CGRectMake(20, 20, 50, 50);
+    _button1 = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button1.backgroundColor = [UIColor blueColor];
+    [_button1 addTarget:self action:@selector(buttonPresses:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_button1];
+    
 }
+
+- (IBAction)buttonPresses:(id)sender
+{
+    NSLog(@"Button 1 was pressed.");
+}
+
 
 - (void)didReceiveMemoryWarning
 {
